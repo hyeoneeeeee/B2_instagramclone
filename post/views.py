0c9a2c755c6f4b40ae54c3418976efe27371dc2f
DongIkkk <w8h0412@gmail.com>
2022-10-02 19:26:50 +0900

    로그인 후 게시물조회 페이지와 연결완료

--- post/views.py
+++ post/views.py
@@ -5,10 +5,13 @@ from django.contrib import auth
 
 # Create your views here.
 def home(request):
-    # if 로그인 되어 있지 않은 상태라면 sign-in페이지로
-    # 로그인 되어있다면 아래 게시글 조회페이지
-    all_post = PostModel.objects.all().order_by('-created_at')
-    return render(request, 'post.html', {'posts': all_post})
+    if request.method == 'GET':
+        user = request.user.is_authenticated
+        if user:
+            all_post = PostModel.objects.all().order_by('-created_at')
+            return render(request, 'post/post.html', {'posts': all_post})
+        else:
+            return redirect('sign_in')
 
 
 def write(request): # 게시글 작성 페이지
@@ -29,10 +32,10 @@ def write(request): # 게시글 작성 페이지
 
 
 def my_profile(request): #나의 프로필 페이지
-    return render(request, 'my_profile.html')
+    return render(request, 'post/my_profile.html')
 
 
 def detail_post(request, id):
     my_post = PostModel.objects.get(id=id)
     my_comment = CommentModel.objects.filter(post_id=id).order_by('-created_at')
-    return render(request, 'post_detail.html', {'post': my_post, 'comment': my_comment})
+    return render(request, 'post/post_detail.html', {'post': my_post, 'comment': my_comment})
