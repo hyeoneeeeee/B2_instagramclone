b07e3e5a59b1aa827ae2f7a58381b4ab02fe6d48
DongIkkk <w8h0412@gmail.com>
2022-10-04 21:14:38 +0900

    게시글 상세보기 페이지 모달창 댓글기능

--- post/urls.py
+++ post/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('post/<int:id>', views.detail_post, name='detail-post'),
     path('myprofile/', views.my_profile, name='my-profile'),
     path('post/delete/<int:id>', views.delete_post, name='delete-post'),
+    path('post/comment/<int:id>', views.write_comment, name='write_comment'),
+    path('post/comment/delete/<int:id>', views.delete_comment, name='write_comment'),
 ]
